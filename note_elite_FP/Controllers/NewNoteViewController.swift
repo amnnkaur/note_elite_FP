ec62b9949d23e447d94bfcd3c1b46bfc61ebf402
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-15 18:56:55 -0400

    [Anmol Singh]NewNoteViewController: STT recognized and converted into string

--- note_elite_FP/Controllers/NewNoteViewController.swift
+++ note_elite_FP/Controllers/NewNoteViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Speech
-import AVKit
 
 class NewNoteViewController: UIViewController, SFSpeechRecognizerDelegate {
 
@@ -17,12 +16,10 @@ class NewNoteViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     public var completion: ((String, String) -> Void)?
     
-    
-    let speechRecognizer        = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
-
-    var recognitionRequest      : SFSpeechAudioBufferRecognitionRequest?
-    var recognitionTask         : SFSpeechRecognitionTask?
-    let audioEngine             = AVAudioEngine()
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +27,6 @@ class NewNoteViewController: UIViewController, SFSpeechRecognizerDelegate {
          titleField.becomeFirstResponder()
           navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
         
-        self.setupSpeech()
     }
         @objc func didTapSave() {
         if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
@@ -42,154 +38,47 @@ class NewNoteViewController: UIViewController, SFSpeechRecognizerDelegate {
         
         if audioEngine.isRunning {
                    self.audioEngine.stop()
-                   self.recognitionRequest?.endAudio()
-//                   self.btnStart.isEnabled = false
-//                   self.btnStart.setTitle("Start Recording", for: .normal)
-               } else {
-                   self.startRecording()
-//                   self.btnStart.setTitle("Stop Recording", for: .normal)
+     
+        } else {
+                    self.recordAndRecognizeSpeech()
+
                }
     }
     
+    func recordAndRecognizeSpeech(){
+       let node = audioEngine.inputNode
+           let recordingFormat = node.outputFormat(forBus: 0)
+           node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+               self.request.append(buffer)
+           }
+           
+           audioEngine.prepare()
+           do{
+               try audioEngine.start()
+           } catch {
+               return print(error)
+           }
+           
+           guard let myRecognizer = SFSpeechRecognizer() else {
+               return
+           }
+           
+           if !myRecognizer.isAvailable {
+               return
+           }
+           
+           recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+               if let result = result {
+                   let bestString = result.bestTranscription.formattedString
+                   self.noteField.text = bestString
+               } else if let error =  error{
+                   print(error)
+               }
+           })
+       }
+
     
-    
-    //------------------------------------------------------------------------------
-    // MARK:-
-    // MARK:- Custom Methods
-    //------------------------------------------------------------------------------
 
-    func setupSpeech() {
-
-//        self.btnStart.isEnabled = false
-        self.speechRecognizer?.delegate = self
-
-        SFSpeechRecognizer.requestAuthorization { (authStatus) in
-
-            var isButtonEnabled = false
-
-            switch authStatus {
-            case .authorized:
-                isButtonEnabled = true
-
-            case .denied:
-                isButtonEnabled = false
-                print("User denied access to speech recognition")
-
-            case .restricted:
-                isButtonEnabled = false
-                print("Speech recognition restricted on this device")
-
-            case .notDetermined:
-                isButtonEnabled = false
-                print("Speech recognition not yet authorized")
-            }
-
-            OperationQueue.main.addOperation() {
-//                self.btnStart.isEnabled = isButtonEnabled
-            }
-        }
-    }
-
-    //------------------------------------------------------------------------------
-
-    func startRecording() {
-
-        // Clear all previous session data and cancel task
-        if recognitionTask != nil {
-            recognitionTask?.cancel()
-            recognitionTask = nil
-        }
-
-        // Create instance of audio session to record voice
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSession.Category.record, mode: AVAudioSession.Mode.measurement, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
-            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-        } catch {
-            print("audioSession properties weren't set because of an error.")
-        }
-
-        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-
-        let inputNode = audioEngine.inputNode
-
-        guard let recognitionRequest = recognitionRequest else {
-            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
-        }
-
-        recognitionRequest.shouldReportPartialResults = true
-
-        self.recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
-
-            var isFinal = false
-
-            if result != nil {
-
-//                self.lblText.text = result?.bestTranscription.formattedString
-                isFinal = (result?.isFinal)!
-            }
-
-            if error != nil || isFinal {
-
-                self.audioEngine.stop()
-                inputNode.removeTap(onBus: 0)
-
-                self.recognitionRequest = nil
-                self.recognitionTask = nil
-
-//                self.btnStart.isEnabled = true
-            }
-        })
-
-        let recordingFormat = inputNode.outputFormat(forBus: 0)
-        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
-            self.recognitionRequest?.append(buffer)
-        }
-
-        self.audioEngine.prepare()
-
-        do {
-            try self.audioEngine.start()
-        } catch {
-            print("audioEngine couldn't start because of an error.")
-        }
-
-//        self.lblText.text = "Say something, I'm listening!"
-    }
-
-
-//    func requestTranscribePermissions() {
-//        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
-//            DispatchQueue.main.async {
-//                if authStatus == .authorized {
-//                    print("Good to go!")
-//                } else {
-//                    print("Transcription permission was declined.")
-//                }
-//            }
-//        }
-//    }
-//
-//    func transcribeAudio(url: URL) {
-//        // create a new recognizer and point it at our audio
-//        let recognizer = SFSpeechRecognizer()
-//        let request = SFSpeechURLRecognitionRequest(url: url)
-//
-//        // start recognition!
-//        recognizer?.recognitionTask(with: request) { [unowned self] (result, error) in
-//            // abort if we didn't get any transcription back
-//            guard let result = result else {
-//                print("There was an error: \(error!)")
-//                return
-//            }
-//
-//            // if we got the final transcription back, print it
-//            if result.isFinal {
-//                // pull out the best transcription...
-//                print(result.bestTranscription.formattedString)
-//            }
-//        }
-//    }
 
     /*
     // MARK: - Navigation
@@ -201,20 +90,4 @@ class NewNoteViewController: UIViewController, SFSpeechRecognizerDelegate {
     }
     */
 
-}
-
-
-
-// MARK:- SFSpeechRecognizerDelegate Methods
-//------------------------------------------------------------------------------
-
-extension ViewController: SFSpeechRecognizerDelegate {
-
-    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
-        if available {
-//            self.btnStart.isEnabled = true
-        } else {
-//            self.btnStart.isEnabled = false
-        }
-    }
 }
